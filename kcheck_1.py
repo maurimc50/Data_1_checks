726e67698fe5972659bd363286a8fba11a8666f9
maurimc50 <122497726+maurimc50@users.noreply.github.com>
2023-02-19 04:04:45 -0500

    Knowledge Check 1

---
+++ kcheck_1.py
@@ -0,0 +1,21 @@
+# Hello world
+print('Hello World')
+
+# Create List
+automakers = ['Toyota', 'Ford', 'Acura', "Tesla", "BMW", "Subaru", "Dodge"]
+# print Tesla
+print(automakers[3])
+
+# Create Dictionary
+LaptopSpecs = {
+    "brand": "HP",
+    "Screen Size": "15.6",
+    "Processor": "AMD",
+    "Memory": "12GB",
+    "OS": "Windows"
+}
+print(LaptopSpecs['Screen Size'])
+
+# Tuple
+T1 = (10.2, 'Phyton', [1,2,3], 'Java')
+print(T1[2])
